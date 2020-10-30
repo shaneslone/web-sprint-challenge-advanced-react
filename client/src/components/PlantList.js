c305aea173b08e4eb223eb2d69853e74f02898ab
Shane Slone <slone.shane@gmail.com>
2020-10-30 13:14:49 -0400

    added filter to plants list

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -5,6 +5,8 @@ export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
     plants: [],
+    search: '',
+    unfilteredPlants: [],
   };
 
   // when the component mounts:
@@ -12,36 +14,72 @@ export default class PlantList extends Component {
   //   - set the returned plants array to this.state.plants
   componentDidMount() {
     axios.get('http://localhost:3333/plants').then(res => {
-      this.setState({ plants: res.data.plantsData });
+      this.setState({
+        plants: res.data.plantsData,
+        unfilteredPlants: res.data.plantsData,
+      });
+      console.log(this.state.plants);
     });
   }
 
+  handelChange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      plants: this.state.plants.filter(plant =>
+        plant.name.toLowerCase().includes(this.state.search.toLowerCase())
+      ),
+    });
+  };
+
+  handleClear = e => {
+    e.preventDefault();
+    this.setState({ plants: this.state.unfilteredPlants, search: '' });
+  };
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
-      <main className='plant-list'>
-        {this.state?.plants?.map(plant => (
-          <div className='plant-card' key={plant.id} data-testid='plant-card'>
-            <img className='plant-image' src={plant.img} alt={plant.name} />
-            <div className='plant-details'>
-              <h2 className='plant-name'>{plant.name}</h2>
-              <p className='plant-scientific-name'>{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className='plant-bottom-row'>
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
+      <>
+        <form>
+          <label>
+            Filter By Name:
+            <input
+              name='filter'
+              type='text'
+              value={this.state.search}
+              onChange={this.handelChange}
+            />
+          </label>
+          <button onClick={this.handleSubmit}>Search</button>
+          <button onClick={this.handleClear}>Clear Search</button>
+        </form>
+        <main className='plant-list'>
+          {this.state?.plants?.map(plant => (
+            <div className='plant-card' key={plant.id} data-testid='plant-card'>
+              <img className='plant-image' src={plant.img} alt={plant.name} />
+              <div className='plant-details'>
+                <h2 className='plant-name'>{plant.name}</h2>
+                <p className='plant-scientific-name'>{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className='plant-bottom-row'>
+                  <p>${plant.price}</p>
+                  <p>☀️ {plant.light}</p>
+                  <p>💦 {plant.watering}x/month</p>
+                </div>
+                <button
+                  className='plant-button'
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
               </div>
-              <button
-                className='plant-button'
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
             </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </>
     );
   }
 }
