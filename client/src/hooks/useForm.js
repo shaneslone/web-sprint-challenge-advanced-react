998bab0a0b4ccd25b93e2926c8150da98801fb9d
Shane Slone <slone.shane@gmail.com>
2020-10-30 11:36:20 -0400

    created useForm hook and added API call on componentDidMount()

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,20 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = initialValues => {
+  const [value, setValue] = useState(initialValues);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleChanges = e => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [value, showSuccessMessage, handleChanges, handleSubmit];
+};
